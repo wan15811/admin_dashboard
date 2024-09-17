0bdc03a3aeda98e575a0c3d3724832caf70d125f
Wan <58760263+wan15811@users.noreply.github.com>
2024-09-18 02:10:12 +0700

    a

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss"
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>Gold Store</span>
+      <span>Gold Store Admin</span>
+    </div>
+  )
+}
+
+export default Footer
